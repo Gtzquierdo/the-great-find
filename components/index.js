ca3620a8e0046d895aab7bfbc9e94e4160f34378
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2022-07-09 22:40:58 -0400

    corrected error

--- components/index.js
+++ components/index.js
@@ -2,6 +2,6 @@ export { default as FooterBanner } from './FooterBanner';
 export { default as HeroBanner } from './HeroBanner';
 export { default as Cart } from './Cart';
 export { default as Footer } from './Footer';
-export { default as Header } from './Header';  
+export { default as Navbar } from './Navbar';
 export { default as Product } from './Product';
 export { default as Layout } from './Layout';
